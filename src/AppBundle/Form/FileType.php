73ed9396a15dbf696e1aed08d82a464165ee857a
Makdessi Alex <alex.makdessi@me.com>
2016-01-10 18:24:10 +0100

    fix label collection

--- src/AppBundle/Form/FileType.php
+++ src/AppBundle/Form/FileType.php
@@ -4,6 +4,8 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FileType extends AbstractType
@@ -17,7 +19,11 @@ class FileType extends AbstractType
         ;
     }
 
-
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['name'] = '';
+    }
+    
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
